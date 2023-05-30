64482d4893c64ef72c5298cbb8946f3198423e9d
beqa <beqa.oqropiridze1994@gmail.com>
2023-05-30 18:03:50 +0400

    qq

--- src/pages/AgencyHeader.js
+++ src/pages/AgencyHeader.js
@@ -17,7 +17,7 @@ export const AgencyHeader = () => {
 
           <div className="relative">
             <nav className="absolute top-0 left-0 right-0 z-10 border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-              <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-8">
+              <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-8">
                 <Link to="/agency" className="flex items-center">
                   <img src={logo} className="h-6 mr-3" alt="Logo" />
                 </Link>
@@ -98,7 +98,7 @@ export const AgencyHeader = () => {
               <img src={Cov} alt="Cover" className="max-w-full object-cover lampimg" />
             </div>
 
-            <div className="a-homediv">
+            <div className="a-homediv relative">
                 <h3 className="text-white font-extralight text-xl mb-3">We strongly Believe that</h3>
                 <h1 className="text-white font-bold mb-8 text-6xl">IDEAS MATTER</h1>
                 <button type="button" className="readmorebtn text-white w-48 hover:underline decoration-1 bg-violet-500 hover:bg-violet-800 focus:ring-4 focus:ring-violet-300 font-medium rounded-3xl px-2 py-2.5 mr-2 mb-2 dark:bg-violet-600 dark:hover:bg-violet-700 focus:outline-none dark:focus:ring-violet-800">Read More</button>
--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -4,26 +4,28 @@ import logo from '../assets/logo.svg';
 export const Footer = () => {
 
     return (
-        <footer className=' bg-black h-96 mx-auto w-full max-w-screen-2xl absolute'>
-            <img className='h-6 w-40 relative top-14' src={logo} alt='Logo' />
-            <div className='relative top-20 mx-auto p-8 flex justify-between footeri'>
+        <footer className=' bg-black h-96 mx-auto w-full absolute'>
+            <div className='max-w-screen-xl mx-auto'>
+                <img className='h-6 w-40 relative top-14' src={logo} alt='Logo' />
+                
+                <div className='relative top-20 mx-auto p-8 flex justify-between'>
+                    <div className='flex flex-col text-white font-light text-lg'>
+                        <p className='mb-8'>Home</p>
+                        <p className='mb-8'>About Us</p>
+                        <p>Location</p>
+                    </div>
 
-                <div className='flex flex-col text-white font-light text-lg'>
-                    <p className='mb-8'>Home</p>
-                    <p className='mb-8'>About Us</p>
-                    <p>Location</p>
-                </div>
+                    <div className='flex flex-col mr-64 footer-sec2 text-white font-light text-lg'>
+                        <p className='mb-8'>Services</p>
+                        <p className='mb-8'>Contact</p>
+                    </div>
 
-                <div className='flex flex-col mr-64 text-white font-light text-lg'>
-                    <p className='mb-8'>Services</p>
-                    <p className='mb-8'>Contact</p>
-                </div>
-
-                <div className='flex flex-col text-white font-light text-lg justify-end'>
-                    <p className='mb-8 self-end'>News</p>
-                    <p className='mb-8 self-end'>FAQ</p>
-                    <div className='bg-white w-52 h-7 rounded-3xl flex justify-end'>
-                        <button type='button' className='bg-violet-500 rounded-3xl pr-3 pl-3'>Send</button>
+                    <div className='flex flex-col footer-last-sec text-white font-light text-lg justify-end'>
+                        <p className='mb-8 self-end'>News</p>
+                        <p className='mb-8 self-end'>FAQ</p>
+                        <div className='bg-white w-52 h-7 rounded-3xl flex justify-end'>
+                            <button type='button' className='bg-violet-500 rounded-3xl pr-3 pl-3'>Send</button>
+                        </div>
                     </div>
                 </div>
 
