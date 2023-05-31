0e81e02387c04ec08b12904dcf46d28eb0b08794
beqa <beqa.oqropiridze1994@gmail.com>
2023-05-31 20:29:45 +0400

    ko

--- src/pages/Agency.js
+++ src/pages/Agency.js
@@ -18,13 +18,13 @@ export const Agency = () => {
     <div>
 
       <div className="Cover-wrapper">
-        <img className="absolute Cover-img 2xl:w-full" src={Cover} alt="Cover" />
+        <img className="absolute Cover-img" src={Cover} alt="Cover" />
 
-        <div className="relative 2xl:max-w-screen-2xl 2xl:mx-auto">
+        <div className="relative 2xl:max-w-screen-2xl 2xl:mx-auto 2xl:px-8">
           <Header />
         </div>
 
-        <div className="relative flex flex-col items-start max-w-screen-xl p-4 mt-32 mx-auto lg:px-10 2xl:max-w-screen-2xl 2xl:items-start readMore-wrapper">
+        <div className="relative flex flex-col items-start max-w-screen-xl p-4 mt-32 mx-auto lg:px-10 readMore-wrapper 2xl:max-w-screen-2xl 2xl:mx-auto 2xl:px-8 ">
           <h3 className="text-white we-strongly font-extralight text-xl mb-3">We strongly Believe that</h3>
           <h1 className="text-white ideas-matter font-bold mb-8 text-6xl">IDEAS MATTER</h1>
           <button type="button" className="mt-12 readMore-btn text-white w-48 hover:underline decoration-1 bg-violet-500 hover:bg-violet-800 focus:ring-4 focus:ring-violet-300 font-medium rounded-3xl px-2 py-2.5 mr-2 mb-2 dark:bg-violet-600 dark:hover:bg-violet-700 focus:outline-none dark:focus:ring-violet-800">Read More</button>
@@ -87,10 +87,10 @@ export const Agency = () => {
           </div>
         </div>
       </div>
-      <div className="2xl:max-w-screen-2xl 2xl:mx-auto">
+      <div className="2xl:max-w-screen-2xl 2xl:mx-auto 2xl:px-8">
         <Footer />
       </div>
 
     </div>
   )
-}
+};
