b3c72bb9266b33f788089cfd4c1e9e85ad01e322
beqa <beqa.oqropiridze1994@gmail.com>
2023-05-31 20:47:42 +0400

    q

--- src/pages/Agency.js
+++ src/pages/Agency.js
@@ -20,11 +20,11 @@ export const Agency = () => {
       <div className="Cover-wrapper">
         <img className="absolute Cover-img" src={Cover} alt="Cover" />
 
-        <div className="relative 2xl:max-w-screen-2xl 2xl:mx-auto 2xl:px-8">
+        <div className="relative">
           <Header />
         </div>
 
-        <div className="relative flex flex-col items-start max-w-screen-xl p-4 mt-32 mx-auto lg:px-10 readMore-wrapper 2xl:max-w-screen-2xl 2xl:mx-auto 2xl:px-8 ">
+        <div className="relative flex flex-col items-start max-w-screen-xl p-4 mt-32 mx-auto lg:px-10 readMore-wrapper ">
           <h3 className="text-white we-strongly font-extralight text-xl mb-3">We strongly Believe that</h3>
           <h1 className="text-white ideas-matter font-bold mb-8 text-6xl">IDEAS MATTER</h1>
           <button type="button" className="mt-12 readMore-btn text-white w-48 hover:underline decoration-1 bg-violet-500 hover:bg-violet-800 focus:ring-4 focus:ring-violet-300 font-medium rounded-3xl px-2 py-2.5 mr-2 mb-2 dark:bg-violet-600 dark:hover:bg-violet-700 focus:outline-none dark:focus:ring-violet-800">Read More</button>
@@ -47,7 +47,7 @@ export const Agency = () => {
       </div>
 
       <div className="w-full iconsWrapper">
-        <div className="max-w-screen-xl mx-auto 2xl:max-w-screen-2xl">
+        <div className="max-w-screen-xl mx-auto">
           <p className='pt-16 pb-24 text-white font-extralight text-2xl text-center ourServices'>Our Services</p>
 
           <div className='flex justify-around iconsdivi1'>
@@ -87,7 +87,7 @@ export const Agency = () => {
           </div>
         </div>
       </div>
-      <div className="2xl:max-w-screen-2xl 2xl:mx-auto 2xl:px-8">
+      <div>
         <Footer />
       </div>
 
